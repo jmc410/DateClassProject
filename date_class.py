ab7ff9e0bf352a5bae752686dae03d61fd6d7efd
JK <jmc41024@email.vccs.edu>
2026-09-13 13:12:16 -0400

    Created constructor and properties.

--- date_class.py
+++ date_class.py
@@ -1,10 +1,19 @@
 from datetime import date
 import calendar
 
+class Date:
+    def __init__(self, month: int = 1, day: int = 1, year: int = 1900) -> None:
+        self.__date = date(year, month, day)
+        
+    @property
+    def month(self):
+        return self.__date.month
 
+    @property
+    def day(self):
+        return self.__date.day
 
-
-
-
-
-
+    @property
+    def year(self):
+        return self.__date.year
+        
