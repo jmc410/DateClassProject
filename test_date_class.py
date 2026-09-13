38c3bc06cdab0ce314ec4e542c5e7af413b47694
JK <jmc41024@email.vccs.edu>
2026-09-13 13:50:21 -0400

    Created unittest to test for default day.

--- test_date_class.py
+++ test_date_class.py
@@ -1,5 +1,12 @@
 import unittest
-from date_class import date
+from date_class import Date
 
 
 
+class DayTest(unittest.TestCase):
+    def test_day(self):
+        default_day = Date()
+        self.assertEqual(default_day.day,1)
+
+
+# python -m unittest, this is the command to run all unittests. (Use in terminal command line)
