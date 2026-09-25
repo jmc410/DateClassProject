df023cad5bf74bc040f29d580b61fb771b314fc2
JK <jmc41024@email.vccs.edu>
2026-09-24 23:22:21 -0400

    Minor changes

--- test_date_class.py
+++ test_date_class.py
@@ -199,7 +199,7 @@ class UnsupportedSubtraction(unittest.TestCase):
         test_date = Date(4, 18, 2014)
 
         with self.assertRaises(TypeError):
-            test_date - "Invalid Date."
+            test_date - "Invalid Test."
 
 
 
